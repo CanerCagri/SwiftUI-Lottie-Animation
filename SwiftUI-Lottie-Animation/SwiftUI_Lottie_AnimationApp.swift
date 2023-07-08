6a6439fdd81a47816f8d9a526be0513a1e0afbf6
CanerCagri <canerrcagrii@gmail.com>
2023-07-08 21:20:31 +0300

    Initial Commit

---
+++ SwiftUI-Lottie-Animation/SwiftUI_Lottie_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Lottie_AnimationApp.swift
+//  SwiftUI-Lottie-Animation
+//
+//  Created by Caner Çağrı on 8.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Lottie_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
