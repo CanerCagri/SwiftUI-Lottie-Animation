90e1fca2f5a3b7673a8a6dfa22d1ed5d02a865e6
CanerCagri <canerrcagrii@gmail.com>
2023-07-08 21:32:11 +0300

    Lottie library added with SPM, and implemented.

--- SwiftUI-Lottie-Animation/ContentView.swift
+++ SwiftUI-Lottie-Animation/ContentView.swift
@@ -6,14 +6,16 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+  
+            Text("Caner Cagri")
+            
+            LottieView(lottieFile: "animation")
         }
         .padding()
     }
@@ -22,5 +24,32 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct LottieView: UIViewRepresentable {
+    let lottieFile: String
+ 
+    var animationView = LottieAnimationView()
+ 
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView(frame: .zero)
+ 
+        animationView.animation = LottieAnimation.named(lottieFile)
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+ 
+        view.addSubview(animationView)
+ 
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        animationView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+ 
+        return view
+    }
+ 
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+ 
     }
 }
